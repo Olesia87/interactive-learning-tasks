de4670af90c9a4f3d5739c5f2d97e52d9ca28770
Olesia <olesia.dm21@gmail.com>
2021-08-03 06:30:13 +0000

    security_group

---
+++ sec_group/output.tf
@@ -0,0 +1,13 @@
+output  "sec_group_id"{
+value = aws_security_group.allow_tls.id
+}
+
+output "owner_id"{
+value = aws_security_group.allow_tls.owner_id
+
+}
+
+output "arn" {
+value = aws_security_group.allow_tls.arn
+
+}
